da6e6e8296a26fec5006d45b9d340a260957c201
sydneycalcagno <89706903+sydneycalcagno@users.noreply.github.com>
2023-10-23 15:33:02 -0600

    uh

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { camelCase } from "lodash";
+
+console.log(camelCase('hello world'));
+
+
+
+
+
+
+// function component() {
+//     var element = document.createElement('div');
+  
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
